7b0d9767d783aee06e34218bf8a19ca6913ddb9c
Chris Flannery <chris.flannery@koneksahealth.com>
2023-02-06 09:11:56 -0500

    introduce basic functionality

---
+++ src/components/UseFetchComponent.tsx
@@ -0,0 +1,47 @@
+import { FC, useState } from "react";
+import styles from '@/styles/Home.module.css'
+import useFetch from "@/hooks/useFetch";
+
+type Post = {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+}
+
+export const UseFetchComponent: FC = () => {
+  const postIds = [1,2,3,4,5,6,7,8];
+  const [index, setIndex] = useState(0);
+
+  const { error, data } = useFetch<Post>(
+    `https://jsonplaceholder.typicode.com/posts/${postIds[index]}`
+  );
+  
+  const incrementIndex = () => setIndex((i) => i === postIds.length - 1 ? i : i + 1);
+
+  const NextPostButton = () => <button onClick={incrementIndex}>Next Post</button>;
+
+  if (error) {
+    return (
+      <>
+        <p>{error.message}</p>
+        <NextPostButton />
+      </>
+    )
+  }
+  if (!data) {
+    return <p>...loading...</p>;
+  };
+
+
+  return (
+    <div className={styles.center}>
+      <h1>{data.title}</h1>
+      <p>{data.body}</p>
+      {index === postIds.length - 1
+        ? <p>reached end of list</p>
+        : <NextPostButton />
+      }
+    </div>
+  );
+}
